e525c8d0c683cc820ef19449337f8ec0710e339e
Adidya Abimanyu <114373205+AdidyaAbimanyu@users.noreply.github.com>
2025-07-31 10:53:29 +0700

    Update footer.blade.php

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="text-white py-4 mt-5" style="background-color: #27394A">
+    <div class="container text-center">
+        <h5 class="mb-4">Win News</h5>
+        <div class="mb-3">
+            <a href="#" class="btn btn-outline-light btn-lg mx-2 rounded-3">
+                <i class="bi bi-facebook"></i>
+            </a>
+            <a href="#" class="btn btn-outline-light btn-lg mx-2 rounded-3">
+                <i class="bi bi-twitter"></i>
+            </a>
+            <a href="#" class="btn btn-outline-light btn-lg mx-2 rounded-3">
+                <i class="bi bi-instagram"></i>
+            </a>
+            <a href="#" class="btn btn-outline-light btn-lg mx-2 rounded-3">
+                <i class="bi bi-youtube"></i>
+            </a>
+        </div>
+        <small>© 2025 Adidya Abimanyu. All Rights Reserved.</small>
+    </div>
+</footer>
